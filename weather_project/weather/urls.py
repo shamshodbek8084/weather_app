c77a46724fcf3f0bb6d459da39c893a926272194
shamshodbek8084 <erkinmulkamonov@gmail.com>
2025-09-19 16:03:43 +0500

    Shamshodbek wrote views.pys to this project

--- weather_project/weather/urls.py
+++ weather_project/weather/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import WeatherView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', WeatherView.as_view(), name='home'),
 ]
--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views import View
+import requests
 
-# Create your views here.
+class WeatherView(View):
+    template_name = 'weather/home.html'
+
+    def get(self, request):
+        city = request.GET.get('city')
+        weather_data = None
+
+        if city:
+            api_key = "YOUR_API_KEY"  # Replace with your OpenWeatherMap API key
+            url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=en"
+            response = requests.get(url).json()
+
+            if response.get("cod") == 200:
+                weather_data = {
+                    'city': city,
+                    'temperature': response['main']['temp'],
+                    'description': response['weather'][0]['description'],
+                    'icon': response['weather'][0]['icon'],
+                }
+            else:
+                weather_data = {'error': "City not found!"}
+
+        return render(request, self.template_name, {'weather': weather_data})
